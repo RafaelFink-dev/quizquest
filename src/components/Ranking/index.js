c7214229e7f1a740d2d2725e08e91e9069012b68
Rafael Fink <rafaelfinkanselmo99@gmail.com>
2024-07-08 23:02:40 -0300

    Fix bugs

---
+++ src/components/Ranking/index.js
@@ -0,0 +1,35 @@
+import './ranking.css'
+
+export default function Ranking({ listRanking = [] }) {
+
+
+    return (
+        <div className='container-ranking'>
+
+
+            <div className='title-ranking-div'>
+                <div>
+                    <h1>PARTICIPANTE</h1>
+                    <h1>PONTOS</h1>
+                </div>
+            </div>
+
+            <div style={{ color: '#121212' }} className='list-ranking'>
+                {listRanking.map((retorno, index) => (
+                    <div key={retorno.id} className='list-ranking-div'>
+                        <div className='list-ranking-div-posicao'>
+                            <h1 style={{ color: '#121212' }}>{index + 1}</h1>
+                        </div>
+                        <div className='list-ranking-div-nome'>
+                            <h1 style={{ color: '#FFF' }}>{retorno.nomeUsuario.toUpperCase()}</h1>
+                        </div>
+                        <div className='list-ranking-div-pontos'>
+                            <h1>{retorno.pontos ? retorno.pontos : 0}</h1>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import './home.css';
 
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, orderBy, query, limit } from 'firebase/firestore';
 import { db } from '../../services/firebaseConnection';
 import { useEffect, useState } from 'react';
 
 import { FiHome, FiClipboard, FiBarChart } from 'react-icons/fi';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import Ranking from '../../components/Ranking';
 
 import { Link } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ export default function Home() {
     const listRefUsers = collection(db, 'users');
     const listRefQuizes = collection(db, 'quizzes');
     const [users, setUsers] = useState(0);
+    const [usersRanking, setUsersRanking] = useState({});
     const [quizes, setQuizes] = useState(0);
     const [loadUsers, setLoadUsers] = useState(true);
     const [loadQuizes, setLoadQuizes] = useState(true);
@@ -29,6 +31,18 @@ export default function Home() {
             const querySnapshot = await getDocs(listRefUsers)
                 .then((snapshot) => {
 
+                    //let usersRanking = [];
+
+                    snapshot.docs.map((aluno) => {
+                        usersRanking.push({
+                            id: aluno.id,
+                            nomeUsuario: aluno.data().nome,
+                            pontos: aluno.data().pontos
+                        })
+                    })
+
+                    //setUsersRanking(usersRanking);
+                    //console.log(usersRanking)
                     setUsers(snapshot.docs.length);
                     setLoadUsers(false);
 
@@ -59,7 +73,41 @@ export default function Home() {
                 })
         }
 
+        async function loadUsersRanking() {
+            try {
+                // Cria a query para ordenar os documentos pela coluna 'pontos' em ordem decrescente
+                const q = query(listRefUsers, orderBy('pontos', 'desc'), limit(9));
 
+                // Obtém os documentos da query
+                const querySnapshot = await getDocs(q);
+
+                let usersRanking = [];
+
+                // Mapeia os documentos retornados pela query para o array usersRanking
+                querySnapshot.docs.forEach((aluno) => {
+                    usersRanking.push({
+                        id: aluno.id,
+                        nomeUsuario: aluno.data().nome,
+                        pontos: aluno.data().pontos
+                    });
+                });
+
+                // Define o estado com a lista de usuários ordenada
+                setUsersRanking(usersRanking);
+                console.log('Users Ranking:', usersRanking);
+
+                // Define outros estados conforme necessário
+                setUsers(querySnapshot.docs.length);
+                setLoadUsers(false);
+            } catch (e) {
+                console.log(e);
+                setLoadUsers(false);
+                setUsers(0);
+            }
+        }
+
+
+        loadUsersRanking();
         loadUsers();
         loadQuizes();
 
@@ -82,6 +130,15 @@ export default function Home() {
                         <div className='ranking-title'>
                             <FiBarChart size={24} />
                             <h1>CONFIRA O RANKING DOS MELHORES COLOCADOS:</h1>
+                        </div>
+                        <div className='ranking'>
+                            <div className='loading-ranking'>
+                                {loadUsers ? (
+                                    <span style={{ fontSize: 30, marginTop: 30, fontWeight: 'bold' }}>CARREGANDO RANKING...</span>
+                                ) : (
+                                    <Ranking listRanking={usersRanking} />
+                                )}
+                            </div>
                         </div>
                     </div>
 
